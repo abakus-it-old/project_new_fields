096b58909f8c8f4a2e986cc000b4a0883afad5a1
Valentin Thirion <valentinthirion@gmail.com>
2016-03-31 12:07:06 +0200

    Improved description

--- __openerp__.py
+++ __openerp__.py
@@ -7,9 +7,13 @@
     'category': 'Project',
     'description': 
     """
+New fields on Projects
+
 Adds new fields on projects:
+
 - 'description' Text
 - 'attachments'
+This module has been developed by Valentin THIRION @ AbAKUS it-solutions.
     """,
     'data': [
         'view/project_form.xml',
